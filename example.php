0ce0f9b8a5f1289dd74117b7ca3b67221e6c4af0
Nweneuba <136861175+Nweneuba@users.noreply.github.com>
2024-01-05 13:29:58 +0100

    Add files via upload

--- example.php
+++ example.php
@@ -4,8 +4,27 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<style>
+		.main-content{
+			width: 800px;
+			height: 1000px;
+			float: left;
+			background-color: #000;
+		}
+		.sidebar{
+			float: right;
+			width: 300px;
+			height: 1000px;
+			background-color: #00f;
+		}
+	</style>
 </head>
 <body>
-<h1 fi fi-user>ok</h1>
+	<label for="choose payment">
+		<inpu
+	<div class="conainer">
+<div class="main-content"><h1> when are you coming to my house oh flower girlie oo</h1></div>
+<div class="sidebar"></div>
+</div>
 </body>
 </html>
